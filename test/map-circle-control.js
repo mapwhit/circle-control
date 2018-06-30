eb5d91cdc57e181e0e39a25a3d7e66b67795099a
Damian Krzeminski <pirxpilot@furkot.com>
2018-06-30 12:31:49 -0600

    add basic test

--- test/map-circle-control.js
+++ test/map-circle-control.js
@@ -1,9 +1,26 @@
-var should = require('should');
-var mapCircleControl = require('../');
+var makeCircle = require('../');
 
-describe('map-circle-control node module', function () {
-  it('must have at least one test', function () {
-    mapCircleControl();
-    should.fail('Need to write tests.');
+describe('map-circle-control', function () {
+  before(function() {
+    document.body.innerHTML = '<div class="container"></div>';
+  });
+
+  it('create DOM nodes', function () {
+    const map = {
+      getContainer:  () => document.querySelector('.container')
+    };
+
+    let circle = makeCircle();
+    circle.add(map);
+    circle.center = [ 250, 200 ];
+    circle.radius = 175;
+
+
+    let c = document.querySelector('.container .circle');
+
+    c.childNodes.should.have.length(5);
+    c.should.have.property('style');
+    c.style.should.have.property('left', '250px');
+    c.style.should.have.property('top', '200px');
   });
 });
